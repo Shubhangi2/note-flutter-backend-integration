cf40c11cfff16ccdac1f32452f51bdbcfbc87d60
Shubhangi Jadhav <jshubhangi958@gmail.com>
2025-01-01 22:00:21 +0530

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/presentation/note_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(providers:[
+      
+    ] , child: const MyApp(),)
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/presentation/note_provider.dart
+++ lib/presentation/note_provider.dart
@@ -1,1 +1,27 @@
 
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:note_app/data/note_model.dart';
+
+class NoteProvider  with ChangeNotifier{
+   final _dio = Dio();
+
+    List<NoteModel> _noteList = [];
+    List<NoteModel> get noteList => _noteList;
+    
+    void setNoteList(List<NoteModel> list){
+      _noteList = list;
+    }
+
+
+    void getNotes() async{
+        try{
+
+        }catch(e){
+          
+        }
+    }
+
+
+}
